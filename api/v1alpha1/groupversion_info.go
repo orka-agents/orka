c50f33c680a4a2f04d8681db03d34faf0bdf4e84
Sertac Ozercan <sozercan@gmail.com>
2026-06-11 21:21:57 -0600

    Avoid deprecated scheme builder

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -10,8 +10,9 @@ MIT License - see LICENSE file for details.
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
@@ -19,8 +20,23 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "core.orka.ai", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &schemeBuilder{groupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+type schemeBuilder struct {
+	groupVersion schema.GroupVersion
+	objects      []runtime.Object
+}
+
+func (b *schemeBuilder) Register(objects ...runtime.Object) {
+	b.objects = append(b.objects, objects...)
+}
+
+func (b *schemeBuilder) AddToScheme(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(b.groupVersion, b.objects...)
+	metav1.AddToGroupVersion(scheme, b.groupVersion)
+	return nil
+}
